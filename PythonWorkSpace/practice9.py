119742ae5252bb23727f87ebc7f5c0f94c6c0940
chahyunsoo <chahs1012@naver.com>
2023-03-02 22:22:44 +0900

    pickle

--- PythonWorkSpace/practice9.py
+++ PythonWorkSpace/practice9.py
@@ -1,7 +1,11 @@
-score_file=open("score.txt","r",encoding="utf-8")
-lines=score_file.readlines() #list형태로 저장
-for line in lines:
-    print(line,end="")
-    
-score_file.close()
-score_file.close()
+import pickle
+# profile_file=open("profile.picke","wb")
+# profile={"이름":"차현수","나이":24,"취미":["축구","골프","코딩"]}
+# print(profile)
+# pickle.dump(profile,profile_file) #profile에 있는 정보를 file에 저장하는 것
+# profile_file.close()
+
+profile_file=open("profile.picke","rb")
+profile=pickle.load(profile_file) #file에 있는 정보를 profile에 불러오기
+print(profile)
+profile_file.close()
