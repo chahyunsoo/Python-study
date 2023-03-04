2102953f15cb06826148776f55b4171f2be77abc
chahyunsoo <chahs1012@naver.com>
2023-03-04 15:54:38 +0900

    클래스 정리

--- PythonWorkSpace/pr9/practice9.py
+++ PythonWorkSpace/pr9/practice9.py
@@ -47,16 +47,26 @@
 
 
 #####상속#####
+from random import *
+
 class Unit:
     def __init__(self,name,hp,speed):
         self.name=name
         self.hp=hp
         self.speed=speed
-        # print("{0} 유닛이 생성되었습니다".format(self.name))
+        print("{0} 유닛이 생성되었습니다".format(self.name))
         # print("체력 {0},공격력 {1}".format(self.hp,self.damage))
     def move(self,location):
-        print("[지상 유닛 이동]")
         print("{0} : {1} 방향으로 이동합니다. [속도 {2}]".format(self.name,location,self.speed))
+    
+    def damaged(self,damage):
+        print("{0} :{1} 데미지를 입었습니다.".format(self.name,damage))
+        self.hp-=damage
+        print("{0} :현재 체력은 {1} 입니다.".format(self.name,self.hp))
+        if self.hp <=0:
+            print("{0} :파괴되었습니다.".format(self.name))
+
+
 class AttackUnit(Unit): #Unit을 상속
     def __init__(self,name,hp,speed,damage):
         Unit.__init__(self,name,hp,speed)
@@ -66,12 +76,6 @@ class AttackUnit(Unit): #Unit을 상속
         print("{0} :{1} 방향으로 적군을 공격합니다. [공격력 {2}]"\
               .format(self.name,location,self.damage))
         
-    def damaged(self,damage):
-        print("{0} :{1} 데미지를 입었습니다.".format(self.name,damage))
-        self.hp-=damage
-        print("{0} :현재 체력은 {1} 입니다.".format(self.name,self.hp))
-        if self.hp <=0:
-            print("{0} :파괴되었습니다.".format(self.name))
 
 class Flyable:
     def __init__(self,flying_speed):
@@ -85,8 +89,104 @@ class FlyableAttackUnit(AttackUnit,Flyable):
         AttackUnit.__init__(self,name,hp,0,damage)
         Flyable.__init__(self,flying_speed)
 
-valkyrie=FlyableAttackUnit("발키리",200,6,5)
-valkyrie.fly(valkyrie.name,"3시")
+    def move(self,location):
+        self.fly(self.name,location)
+        
+class Marine(AttackUnit):
+    def __init__(self):
+        AttackUnit.__init__(self,"마린",40,1,5)
+
+    def stimpack(self):
+        if self.hp>10:
+            self.hp-=10
+            print("{0} : 스팀팩을 사용합니다(HP 10 감소)".format(self.name))
+        else:
+            print("{0} : 체력이 부족하여 스팀팩을 사용하지 않습니다".format(self.name))
+
+class Tank(AttackUnit):
+    seize_developed =False  #개발 여부
+    def __init__(self):
+        AttackUnit.__init__(self,"탱크",150,1,35)
+        self.seize_mode=False
+
+    def set_seize_mode(self):
+        if Tank.seize_developed==False:
+            return
+        
+        if self.seize_mode==False:
+            print("{0} : 시즈모드로 전환합니다".format(self.name))
+            self.damage*=2
+            self.seize_mode=True
+        else:
+            print("{0} : 시즈모드를 해제합니다".format(self.name))
+            self.damage/=2
+            self.seize_mode=False
 
 
+class Wraith(FlyableAttackUnit):
+    def __init__(self):
+        FlyableAttackUnit.__init__(self,"레이스",80,20,5)
+        self.clocked=False #해제 상태
+    
+    def clocking(self):
+        if self.clocked==True:
+            print("{0} ; 클로킹 모드를 해제합니다".format(self.name))
+            self.clocked=True
+        else: #해제 -> 설정
+            print("{0} : 클로킹 모드를 설정합니다".format(self.name))
+            self.clocked=True
+            
+def game_start():
+    print("[알림] 새로운 게임을 시작합니다")
+def game_over():
+    print("Player : GG")
+    print("[Player] 님이 게임에서 퇴장하였습니다.")
 
+
+#진행
+game_start()
+m1=Marine()
+m2=Marine()
+m3=Marine()
+
+t1=Tank()
+t2=Tank()
+
+w1=Wraith()
+
+#일괄 관리
+attack_units=[]
+attack_units.append(m1)
+attack_units.append(m2)
+attack_units.append(m3)
+attack_units.append(t1)
+attack_units.append(t2)
+attack_units.append(w1)
+
+#모두 이동
+for unit in attack_units:
+    unit.move("1시")
+
+#시즈모드 개발
+Tank.seize_developed=True
+print("[알림] 탱크 시즈 모드 개발이 완료되었습니다")
+
+#공격 모드 준비(마린:스팀팩, 탱크:시즈모드, 레이스:클로킹)
+for unit in attack_units:
+    if isinstance(unit,Marine):
+        unit.stimpack()
+    elif isinstance(unit,Tank):
+        unit.set_seize_mode()
+    elif isinstance(unit,Wraith):
+        unit.clocking()
+
+#모두 공격
+for unit in attack_units:
+    unit.attack("1시")
+
+#모두 피해
+for unit in attack_units:
+    unit.damaged(randint(5,21)) #공격은 랜덤으로 받음 5~20
+
+#게임 종료
+game_over()
